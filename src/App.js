69d336db831f914d53919649c2264e24cd6a428a
Sam Souder <github@samsouder.com>
2017-05-26 12:13:21 -0500

    Base implementation of SimplePhoto

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+// @flow
 import React, { Component } from 'react';
 import SimplePhoto from './SimplePhoto';
 import Datetime from './Datetime';
@@ -5,10 +6,15 @@ import DarkSkyWeather from './DarkSkyWeather';
 import './App.css';
 
 class App extends Component {
+  photos: string[] = [
+    'https://camo.githubusercontent.com/f28b5bc7822f1b7bb28a96d8d09e7d79169248fc/687474703a2f2f692e696d6775722e636f6d2f4a65567164514d2e706e67',
+    'https://d2eip9sf3oo6c2.cloudfront.net/series/covers/000/000/112/half/EGH_banner_RxJSMistakes_Final.png?1495648331',
+    'http://facebook.github.io/flux/img/flux_logo.svg'
+  ];
   render() {
     return (
-      <div className="App">
-        <SimplePhoto />
+      <div>
+        <SimplePhoto photos={this.photos}/>
         <Datetime />
         <DarkSkyWeather />
       </div>
--- src/SimplePhoto.js
+++ src/SimplePhoto.js
@@ -1,11 +1,22 @@
+// @flow
 import React, { Component } from 'react';
 import './SimplePhoto.css';
 
 // process.env.REACT_APP_DROPBOX_API_TOKEN
 
 class SimplePhoto extends Component {
+  props: { photos: string[] };
+
   render() {
-    return <div className="Photo">MY PHOTO HERE</div>;
+    // Pick a random photo
+    const photo: string = this.props.photos[Math.floor(Math.random() * this.props.photos.length)];
+
+    return (
+      <div>
+        <div className="Photo" style={{ backgroundImage: 'url(' + photo + ')' }} />
+        <div className="PhotoBackground" style={{ backgroundImage: 'url(' + photo + ')' }} />
+      </div>
+    );
   }
 }
 
