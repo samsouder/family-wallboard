469bf05a4238cc34aea4012162097e44203167cb
Sam Souder <github@samsouder.com>
2017-05-26 12:31:46 -0500

    Base Datetime implementation

--- src/Datetime.js
+++ src/Datetime.js
@@ -1,23 +1,42 @@
+// @flow
 import React, { Component } from 'react';
-import Moment from 'moment'
+import Moment from 'moment';
+import './Datetime.css';
 
 class Datetime extends Component {
+  timerId: number;
+  state: { date: Date } = { date: new Date() };
+
+  componentDidMount() {
+    this.timerId = setInterval(() => this.tick(), 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerId);
+  }
+
+  tick() {
+    this.setState({ date: new Date() });
+  }
+
   render() {
-    return <div className="Datetime">
-      <div className="time">
-        <span className="hours">{Moment().format('h')}</span>
-        <span className="separator">:</span>
-        <span className="minutes">{Moment().format('m')}</span>
-        <span className="separator">:</span>
-        <span className="seconds">{Moment().format('s')}</span>
-        <span className="ampm">{Moment().format('a')}</span>
+    const date = this.state.date;
+
+    return (
+      <div className="Datetime">
+        <div className="time">
+          <span className="hours">{Moment(date).format('h')}</span>
+          <span className="minutes">{Moment(date).format('mm')}</span>
+          <span className="seconds">{Moment(date).format('s')}</span>
+          <span className="ampm">{Moment(date).format('a')}</span>
+        </div>
+        <div className="date">
+          <span className="dayOfWeek">{Moment(date).format('dddd')}</span>
+          <span className="month">{Moment(date).format('MMM')}</span>
+          <span className="dayOfMonth">{Moment(date).format('Mo')}</span>
+        </div>
       </div>
-      <div className="date">
-        <span className="dayOfWeek">{Moment().format('ddd')}</span>
-        <span className="month">{Moment().format('MMM')}</span>
-        <span className="dayOfMonth">{Moment().format('Mo')}</span>
-      </div>
-    </div>;
+    );
   }
 }
 
