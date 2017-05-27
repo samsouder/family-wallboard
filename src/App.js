8185bac911eea89c2cb6158bc2f641dc4212ae71
Sam Souder <github@samsouder.com>
2017-05-26 23:47:09 -0500

    Crappy working implementation of Dropbox API for photos

--- src/App.js
+++ src/App.js
@@ -6,15 +6,14 @@ import DarkSkyWeather from './DarkSkyWeather';
 import './App.css';
 
 class App extends Component {
-  photos: string[] = [
-    'https://camo.githubusercontent.com/f28b5bc7822f1b7bb28a96d8d09e7d79169248fc/687474703a2f2f692e696d6775722e636f6d2f4a65567164514d2e706e67',
-    'https://d2eip9sf3oo6c2.cloudfront.net/series/covers/000/000/112/half/EGH_banner_RxJSMistakes_Final.png?1495648331',
-    'http://facebook.github.io/flux/img/flux_logo.svg'
-  ];
+  temperatureScale: 'c' | 'f' = 'f';
+
   render() {
     return (
-      <div>
-        <SimplePhoto photos={this.photos}/>
+      <div className="App">
+        <SimplePhoto />
+        <DigitalClock />
+        <DarkSkyWeather scale={this.temperatureScale} />
       </div>
     );
   }
--- src/SimplePhoto.js
+++ src/SimplePhoto.js
@@ -1,20 +1,86 @@
 // @flow
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+import Dropbox from 'dropbox';
+import {includes, sample} from 'lodash'
 import './SimplePhoto.css';
 
-// process.env.REACT_APP_DROPBOX_API_TOKEN
-
 class SimplePhoto extends Component {
-  props: { photos: string[] };
+  state: {
+    photos: string[],
+    url: string
+  };
+  dbx: Dropbox;
+  timerId: number;
+  photoRefreshTime: number;
+
+  constructor(props: {}) {
+    super(props);
+    this.state = {
+      photos: [],
+      url: ''
+    };
+    this.dbx = new Dropbox({accessToken: process.env.REACT_APP_DROPBOX_API_TOKEN});
+    this.photoRefreshTime = 10;
+  }
+
+  componentDidMount() {
+    console.log('componentDidMount');
+    const extensions = ['jpg', 'jpeg'];
+    this.dbx.filesListFolder({path: '/Photos/Family Wallboard', recursive: true}).then((response) => {
+      const imageFiles = response.entries.filter((e) => {
+        return e['.tag'] === 'file' &&
+          includes(extensions, e.name.substr(e.name.lastIndexOf('.') + 1));
+      }).map((e) => e.path_lower);
+
+      // store the photo paths in state
+      console.log('finished getting photos');
+      this.setState({...this.state, photos: imageFiles});
+
+      // start the process of picking a photo and getting its url
+      this.beamMeUpScotty();
+    }).catch((error) => {
+      console.error(error);
+    });
+  }
+
+  componentWillUnmount() {
+    clearTimeout(this.timerId);
+  }
+
+  beamMeUpScotty() {
+    if (this.state.photos.length === 0) {
+      this.timerId = setTimeout(this.beamMeUpScotty.bind(this), this.photoRefreshTime * 1000);
+      return;
+    }
+    // get random photo
+    const path = sample(this.state.photos)
+    this.getPhotoUrl(path);
+  }
+
+  getPhotoUrl(path: string) {
+    this.dbx.filesGetTemporaryLink({path: path}).then(({link}) => {
+      this.setState({...this.state, url: link});
+    }).catch((error) => {
+      console.error(error);
+    }).then(() => {
+      this.timerId = setTimeout(this.beamMeUpScotty.bind(this), this.photoRefreshTime * 1000);
+      console.log('done getting photo url -- waiting ' + this.photoRefreshTime + ' seconds to try new photo');
+    });
+  }
 
   render() {
-    // Pick a random photo
-    const photo: string = this.props.photos[Math.floor(Math.random() * this.props.photos.length)];
+    if (!this.state.url) {
+      return <div>{'Loading photos...'}</div>;
+    }
 
     return (
       <div>
-        <div className="Photo" style={{ backgroundImage: 'url(' + photo + ')' }} />
-        <div className="PhotoBackground" style={{ backgroundImage: 'url(' + photo + ')' }} />
+        <div className="Photo" style={{
+          backgroundImage: 'url(' + this.state.url + ')'
+        }}/>
+        <div className="PhotoBackground" style={{
+          backgroundImage: 'url(' + this.state.url + ')'
+        }}/>
       </div>
     );
   }
