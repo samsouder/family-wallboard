762ac5d158f7f10501421e266cade35edc88e1e9
Sam Souder <github@samsouder.com>
2017-05-27 01:33:49 -0500

    Remove weather component for now

--- src/App.js
+++ src/App.js
@@ -2,18 +2,14 @@
 import React, { Component } from 'react';
 import SimplePhoto from './SimplePhoto';
 import DigitalClock from './DigitalClock';
-import DarkSkyWeather from './DarkSkyWeather';
 import './App.css';
 
 class App extends Component {
-  temperatureScale: 'c' | 'f' = 'f';
-
   render() {
     return (
       <div className="App">
         <SimplePhoto />
         <DigitalClock />
-        <DarkSkyWeather scale={this.temperatureScale} />
       </div>
     );
   }
