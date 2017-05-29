bf7b4fb46c71047b276c46e6277ce81a4aa2c7e6
Sam Souder <github@samsouder.com>
2017-05-28 19:34:38 -0500

    Redo CSS

--- src/App.js
+++ src/App.js
@@ -8,8 +8,8 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <DigitalClock />
         <SimplePhoto />
+        <DigitalClock />
       </div>
     );
   }
