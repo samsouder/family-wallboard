36c539a35874f01538f3b924b4cf655f56385248
Sam Souder <samsouder@gmail.com>
2018-09-25 18:37:16 -0500

    Break out EXIF loading into its own component with a reference to the current image node

---
+++ src/PhotoExif.js
@@ -0,0 +1,47 @@
+// @flow
+/* eslint no-console: "off" */
+import React, { PureComponent } from "react";
+import exif from "exif-js";
+import moment from "moment";
+
+class PhotoExif extends PureComponent {
+  state: {
+    date: string
+  } = { date: null };
+
+  componentDidMount() {
+    this.props.imageRef.current.onload = () => this.updateExifDataDisplay(this.props.imageRef.current);
+  }
+
+  updateExifDataDisplay(imageElement: HTMLImageElement) {
+    console.log("[PhotoExif]", "Finding original date time of image", imageElement.src);
+
+    // Bust the exif-js cache as the image element is being re-used
+    imageElement.exifdata = null;
+
+    const that = this;
+    exif.getData(imageElement, function() {
+      const originalDateTime = exif.getTag(this, "DateTimeOriginal");
+      console.log("[PhotoExif]", "Original date time of image", originalDateTime);
+
+      const date = moment(originalDateTime, "YYYY:MM:DD HH:mm:ss");
+      if (!date.isValid()) {
+        console.log("[PhotoExif] ** Invalid date found **", originalDateTime);
+      }
+
+      that.setState({ date: date.format("MMM 'YY") });
+    });
+  }
+
+  render() {
+    console.log("[PhotoExif]", "Rendering EXIF data...");
+
+    if (!this.state.date) {
+      return null;
+    }
+
+    return <div className="PhotoExif">{this.state.date}</div>;
+  }
+}
+
+export default PhotoExif;
--- src/SimpleRandomPhoto.js
+++ src/SimpleRandomPhoto.js
@@ -1,18 +1,18 @@
 // @flow
 /* eslint no-console: "off" */
-import React, { Component } from "react";
+import React, { PureComponent } from "react";
 import axios from "axios";
-import exif from "exif-js";
-import moment from "moment";
 import Loader from "./Loader";
+import PhotoExif from "./PhotoExif.js";
 import "./SimpleRandomPhoto.css";
 
-class SimpleRandomPhoto extends Component {
+class SimpleRandomPhoto extends PureComponent {
   state: {
     currentPhoto: string,
     nextPhoto: string
   };
   nextPhotoUrlTimer: number;
+  currentPhotoRef;
 
   static defaultProps = {
     photoUrl: "http://localhost:5000/",
@@ -25,6 +25,8 @@ class SimpleRandomPhoto extends Component {
       currentPhoto: "",
       nextPhoto: ""
     };
+
+    this.currentPhotoRef = React.createRef();
   }
 
   componentDidMount() {
@@ -61,55 +63,27 @@ class SimpleRandomPhoto extends Component {
     );
   }
 
-  handleMainPhotoLoaded(event) {
-    // Copy loaded image into background photo holder
-    let backgroundPhotoContainer = document.getElementsByClassName("PhotoBackgroundContainer")[0];
-    let backgroundImage = event.target.cloneNode();
-    backgroundImage.className = "PhotoBackground";
-    backgroundPhotoContainer.appendChild(backgroundImage);
-
-    console.log("Appended new background photo");
-
-    // Update Exif data for main photo
-    this.handleMainPhotoExifUpdate(event);
-  }
-
   handleNextPhotoLoaded(event) {
     console.log(
-      "Next image has loaded, waiting " + this.props.photoRefreshTime + " seconds to switch to it",
+      "[SimpleRandomPhoto] Next image has loaded, waiting " + this.props.photoRefreshTime + " seconds to switch to it",
       event.target
     );
     this.nextPhotoUrlTimer = setTimeout(this.handleNextPhotoSwitch.bind(this), this.props.photoRefreshTime * 1000);
   }
 
-  handleMainPhotoExifUpdate(event) {
-    console.log("Main photo loaded", event.target);
-    this.updateExifDataDisplay(event.target);
+  handleNextPhotoError(event) {
+    console.log("[SimpleRandomPhoto] Next image was not loaded due to error", event);
+    this.nextPhotoUrlTimer = setTimeout(this.handleNextPhotoSwitch.bind(this), this.props.photoRefreshTime * 1000);
   }
 
-  updateExifDataDisplay(imageElement: HTMLImageElement) {
-    let exifElement = document.getElementsByClassName("PhotoExif")[0];
-    exifElement.hidden = true;
-
-    console.log("Finding original date time of image", imageElement);
-
-    // Bust the exif-js cache as the image element is being re-used
-    imageElement.exifdata = null;
-
-    exif.getData(imageElement, function() {
-      let originalDateTime = exif.getTag(this, "DateTimeOriginal");
-      console.log("Original date time of image", originalDateTime);
-
-      let date = moment(originalDateTime, "YYYY:MM:DD HH:mm:ss");
-      if (date.isValid()) {
-        exifElement.innerHTML = date.format("MMM 'YY");
-        exifElement.hidden = false;
-      }
-    });
+  handleCurrentPhotoLoaded() {
+    console.log("[SimpleRandomPhoto] Current image has loaded");
+    // Force re-render to get build up the EXIF data area
+    this.forceUpdate();
   }
 
   render() {
-    console.log("Rendering...");
+    console.log("[SimpleRandomPhoto] Rendering...");
 
     const photosReady = this.state.currentPhoto && this.state.nextPhoto;
 
@@ -123,14 +97,14 @@ class SimpleRandomPhoto extends Component {
           className="NextPhoto"
           src={this.state.nextPhoto}
           onLoad={this.handleNextPhotoLoaded.bind(this)}
-          onError={this.handleNextPhotoLoaded.bind(this)}
+          onError={this.handleNextPhotoError.bind(this)}
           alt=""
         />
         <div className="PhotoBackgroundContainer" />
         <div className="Photo" style={{ visibility: "visible" }}>
-          <img src={this.state.currentPhoto} onLoad={this.handleMainPhotoLoaded.bind(this)} alt="" />
+          <img ref={this.currentPhotoRef} src={this.state.currentPhoto} alt="" />
         </div>
-        <div className="PhotoExif" />
+        <PhotoExif imageRef={this.currentPhotoRef} />
       </div>
     );
   }
