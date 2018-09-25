ef5a368b88c77670e70ef515612a35871e504de7
Sam Souder <samsouder@gmail.com>
2018-09-25 18:35:35 -0500

    Convert app to functional component

--- src/App.js
+++ src/App.js
@@ -1,31 +1,27 @@
 // @flow
-import React, { Component } from "react";
+import React, { PureComponent } from "react";
 import SimpleRandomPhoto from "./SimpleRandomPhoto";
 import DigitalClock from "./DigitalClock";
 import DarkSkyWeather from "./DarkSkyWeather";
 import "./App.css";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <SimpleRandomPhoto
-          photoRefreshTime={((process.env.REACT_APP_PHOTO_REFRESH_INTERVAL_IN_SECONDS: any): number)}
-          photoUrl={((process.env.REACT_APP_PHOTO_URL: any): string)}
-        />
-        <DigitalClock />
-        {/* Refresh time is in minutes (Note DarkSky has a 1000 requests per 24 hours limit) */}
-        <DarkSkyWeather
-          apiToken={((process.env.REACT_APP_DARKSKY_API_TOKEN: any): string)}
-          refreshTime={((process.env.REACT_APP_WEATHER_REFRESH_INTERVAL_IN_MINUTES: any): number)}
-          location={{
-            latitude: ((process.env.REACT_APP_DARKSKY_LOCATION_LATITUDE: any): number),
-            longitude: ((process.env.REACT_APP_DARKSKY_LOCATION_LONGITUDE: any): number)
-          }}
-        />
-      </div>
-    );
-  }
-}
+const App = () => (
+  <div className="App">
+    <SimpleRandomPhoto
+      photoRefreshTime={((process.env.REACT_APP_PHOTO_REFRESH_INTERVAL_IN_SECONDS: any): number)}
+      photoUrl={((process.env.REACT_APP_PHOTO_URL: any): string)}
+    />
+    <DigitalClock />
+    {/* Refresh time is in minutes (Note DarkSky has a 1000 requests per 24 hours limit) */}
+    <DarkSkyWeather
+      apiToken={((process.env.REACT_APP_DARKSKY_API_TOKEN: any): string)}
+      refreshTime={((process.env.REACT_APP_WEATHER_REFRESH_INTERVAL_IN_MINUTES: any): number)}
+      location={{
+        latitude: ((process.env.REACT_APP_DARKSKY_LOCATION_LATITUDE: any): number),
+        longitude: ((process.env.REACT_APP_DARKSKY_LOCATION_LONGITUDE: any): number)
+      }}
+    />
+  </div>
+);
 
 export default App;
