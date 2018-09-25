6298266af67b9f0f63e8697d2802ab741c4083f8
Sam Souder <samsouder@gmail.com>
2018-09-25 18:36:09 -0500

    Revamp clock to increase performance

--- src/DigitalClock.js
+++ src/DigitalClock.js
@@ -1,58 +1,42 @@
 // @flow
-import React, { Component } from 'react';
-import Moment from 'moment';
-import './DigitalClock.css';
+import React, { PureComponent } from "react";
+import Moment from "moment";
+import "./DigitalClock.css";
 
-class DigitalClock extends Component {
+class DigitalClock extends PureComponent {
   timerId: number;
-  displayedDate: Date;
-  state: { date: Date } = { date: new Date() };
+  state: { date: string } = { date: new Moment() };
 
   componentDidMount() {
-    this.timerId = setInterval(() => this.tick(), 5000);
-    this.displayedDate = new Date();
+    this.timerId = setInterval(() => this.tick(), 10000);
   }
 
   componentWillUnmount() {
     clearInterval(this.timerId);
   }
 
-  shouldComponentUpdate(nextProps, nextState) {
-    const diff = nextState.date.getMinutes() - this.displayedDate.getMinutes();
-
-    // Do not re-render if it has been less than a minute
-    if (diff < 1) {
-      return false;
-    }
-
-    // Reset the displayed date to the newly set date
-    this.displayedDate = nextState.date;
-
-    return true;
-  }
-
   tick() {
-    this.setState({ date: new Date() });
+    const newTime = Moment();
+    if (newTime.minutes() !== this.state.date.minutes()) {
+      this.setState({ date: newTime });
+    }
   }
 
   render() {
-    // console.log("[DigitalClock] Rendering...");
-    const date = Moment(this.state.date);
-
     return (
       <div className="DigitalClock">
         <div className="time">
-          <span className="hours">{date.format('h')}</span>
-          <span className="minutes">{date.format('mm')}</span>
+          <span className="hours">{this.state.date.format("h")}</span>
+          <span className="minutes">{this.state.date.format("mm")}</span>
           <span className="container">
-            <span className="seconds">{date.format('ss')}</span>
-            <span className="ampm">{date.format('a')}</span>
+            <span className="seconds">{this.state.date.format("ss")}</span>
+            <span className="ampm">{this.state.date.format("a")}</span>
           </span>
         </div>
         <div className="date">
-          <span className="dayOfWeek">{date.format('dddd')}</span>
-          <span className="month">{date.format('MMM')}</span>
-          <span className="dayOfMonth">{date.format('Do')}</span>
+          <span className="dayOfWeek">{this.state.date.format("dddd")}</span>
+          <span className="month">{this.state.date.format("MMM")}</span>
+          <span className="dayOfMonth">{this.state.date.format("Do")}</span>
         </div>
       </div>
     );
