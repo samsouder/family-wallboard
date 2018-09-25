6926d0d6522aa5503daede5496ca5042789e8092
Sam Souder <samsouder@gmail.com>
2018-09-25 12:10:20 -0500

    Optimize digital clock to only update once a minute and fire timing ticks once every 5 seconds

--- src/DigitalClock.js
+++ src/DigitalClock.js
@@ -5,14 +5,30 @@ import './DigitalClock.css';
 
 class DigitalClock extends Component {
   timerId: number;
+  displayedDate: Date;
   state: { date: Date } = { date: new Date() };
 
   componentDidMount() {
-    this.timerId = setInterval(() => this.tick(), 1000);
+    this.timerId = setInterval(() => this.tick(), 5000);
+    this.displayedDate = new Date();
   }
 
   componentWillUnmount() {
     clearInterval(this.timerId);
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    const diff = nextState.date.getMinutes() - this.displayedDate.getMinutes();
+
+    // Do not re-render if it has been less than a minute
+    if (diff < 1) {
+      return false;
+    }
+
+    // Reset the displayed date to the newly set date
+    this.displayedDate = nextState.date;
+
+    return true;
   }
 
   tick() {
@@ -20,22 +36,23 @@ class DigitalClock extends Component {
   }
 
   render() {
-    const date = this.state.date;
+    // console.log("[DigitalClock] Rendering...");
+    const date = Moment(this.state.date);
 
     return (
       <div className="DigitalClock">
         <div className="time">
-          <span className="hours">{Moment(date).format('h')}</span>
-          <span className="minutes">{Moment(date).format('mm')}</span>
+          <span className="hours">{date.format('h')}</span>
+          <span className="minutes">{date.format('mm')}</span>
           <span className="container">
-            <span className="seconds">{Moment(date).format('ss')}</span>
-            <span className="ampm">{Moment(date).format('a')}</span>
+            <span className="seconds">{date.format('ss')}</span>
+            <span className="ampm">{date.format('a')}</span>
           </span>
         </div>
         <div className="date">
-          <span className="dayOfWeek">{Moment(date).format('dddd')}</span>
-          <span className="month">{Moment(date).format('MMM')}</span>
-          <span className="dayOfMonth">{Moment(date).format('Do')}</span>
+          <span className="dayOfWeek">{date.format('dddd')}</span>
+          <span className="month">{date.format('MMM')}</span>
+          <span className="dayOfMonth">{date.format('Do')}</span>
         </div>
       </div>
     );
