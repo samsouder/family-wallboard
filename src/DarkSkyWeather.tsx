bcdd7824f1da3c3fdc60a20fc7d2bcac382bb65d
Sam Souder <samsouder@gmail.com>
2019-03-12 10:32:52 -0500

    Convert to tabular layout of weather module

--- src/DarkSkyWeather.tsx
+++ src/DarkSkyWeather.tsx
@@ -90,25 +90,21 @@ export default class DarkSkyWeather extends React.PureComponent<
     }
 
     const forecasts = apiData.daily.data.map((data, i: number) => {
-      let precipSpan: JSX.Element | null = null;
-      if (data.precipProbability > 0) {
-        precipSpan = (
-          <span className="precip">
-            {Math.round(data.precipProbability * 100)}%
-          </span>
-        );
-      }
+      const precip =
+        data.precipProbability > 0
+          ? `${Math.round(data.precipProbability * 100)}%`
+          : "";
 
       return (
-        <div key={i} className="forecast">
-          <span className="day">
+        <tr key={i} className="forecast">
+          <td className="day">
             {i === 0 ? "Today" : Moment.unix(data.time).format("ddd")}
-          </span>
-          <span className="high">{Math.round(data.temperatureMax)}°</span>
-          <span className="low">{Math.round(data.temperatureMin)}°</span>
-          <span className={"wi wi-forecast-io-" + data.icon} />
-          {precipSpan}
-        </div>
+          </td>
+          <td className="high">{Math.round(data.temperatureMax)}°</td>
+          <td className="low">{Math.round(data.temperatureMin)}°</td>
+          <td className="icon"><span className={"wi wi-forecast-io-" + data.icon} /></td>
+          <td className="precip">{precip}</td>
+        </tr>
       );
     });
 
@@ -119,33 +115,67 @@ export default class DarkSkyWeather extends React.PureComponent<
 
     return (
       <div className="DarkSkyWeather">
-        <div className="currentTemperature">
-          {Math.round(apiData.currently.temperature)}°{" "}
-          <i className={"wi wi-forecast-io-" + apiData.currently.icon} />
-        </div>
-        <div className="hourlySummary">{apiData.hourly.summary}</div>
-        <div className="currentPrecip">
-          <span className="probability">
-            {Math.round(apiData.hourly.data[0].precipProbability * 100)}%
-          </span>
-          <span className={"wi " + currentPrecipIcon} />
-        </div>
-        <div className="currentWind">
-          <span className="speed">
-            {Math.round(apiData.currently.windSpeed)}
-          </span>
-          <span className="direction">{apiData.currently.windDirection}</span>
-          <span className="wi wi-strong-wind" />
-        </div>
-        <div className="sunrise">
-          {Moment.unix(apiData.daily.data[0].sunriseTime).format("h:mm")}{" "}
-          <i className="wi wi-sunrise" />
-        </div>
-        <div className="sunset">
-          {Moment.unix(apiData.daily.data[0].sunsetTime).format("h:mm")}{" "}
-          <i className="wi wi-sunset" />
-        </div>
-        <div className="forecasts">{forecasts}</div>
+        <table>
+          <tbody>
+            <tr>
+              <td className="currentTemperature">
+                {Math.round(apiData.currently.temperature)}°{" "}
+              </td>
+              <td className="currentIcon">
+                <i className={"wi wi-forecast-io-" + apiData.currently.icon} />
+              </td>
+            </tr>
+
+            <tr>
+              <td className="hourlySummary" colSpan={2}>
+                {apiData.hourly.summary}
+              </td>
+            </tr>
+
+            <tr className="precipAndWind">
+              <td>
+                <div className="currentPrecip">
+                <span className="probability">
+                  {Math.round(apiData.hourly.data[0].precipProbability * 100)}%
+                </span>
+                <span className={"wi " + currentPrecipIcon} />
+                </div>
+              </td>
+              <td>
+                <div className="currentWind">
+                <span className="speed">
+                  {Math.round(apiData.currently.windSpeed)}
+                </span>
+                <span className="direction">
+                  {apiData.currently.windDirection}
+                </span>
+                <span className="wi wi-strong-wind" />
+                </div>
+              </td>
+            </tr>
+
+            <tr className="sunsetAndSunrise">
+              <td className="sunrise">
+                {Moment.unix(apiData.daily.data[0].sunriseTime).format("h:mm")}{" "}
+                <i className="wi wi-sunrise" />
+              </td>
+              <td className="sunset">
+                {Moment.unix(apiData.daily.data[0].sunsetTime).format("h:mm")}{" "}
+                <i className="wi wi-sunset" />
+              </td>
+            </tr>
+
+            <tr>
+              <td className="forecasts" colSpan={2}>
+                <table>
+                  <tbody>
+                    {forecasts}
+                  </tbody>
+                </table>
+              </td>
+            </tr>
+          </tbody>
+        </table>
       </div>
     );
   }
